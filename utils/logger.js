a3e5727ce52dd0ac87f9fca71c1e27aaeb58d439
xWOLXz <kvin.vera@gmail.com>
2025-08-09 07:40:24 -0500

    Update logger.js

--- utils/logger.js
+++ utils/logger.js
@@ -1,4 +1,4 @@
-// albionsito-backend2/utils/logger.js
+// utils/logger.js
 function info(msg) {
   console.log(`[INFO] ${new Date().toISOString()} - ${msg}`);
 }
@@ -7,4 +7,4 @@ function error(msg) {
   console.error(`[ERROR] ${new Date().toISOString()} - ${msg}`);
 }
 
-export default { info, error };
+module.exports = { info, error };
