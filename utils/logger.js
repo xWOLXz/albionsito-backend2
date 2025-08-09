34a3c221ce617920092148ac9fde7cfde799deaf
xWOLXz <kvin.vera@gmail.com>
2025-08-09 12:26:06 -0500

    Update logger.js

--- utils/logger.js
+++ utils/logger.js
@@ -1,10 +1,6 @@
-// utils/logger.js
-function info(msg) {
-    console.log(`[INFO] ${new Date().toISOString()} - ${msg}`);
-}
+const logger = {
+  info: (...params) => console.log(...params),
+  error: (...params) => console.error(...params),
+};
 
-function error(msg) {
-    console.error(`[ERROR] ${new Date().toISOString()} - ${msg}`);
-}
-
-module.exports = { info, error };
+module.exports = logger;
