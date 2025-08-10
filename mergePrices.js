834c40663e843aa9da44dcbbd33bf11454c71d43
xWOLXz <kvin.vera@gmail.com>
2025-08-10 11:55:32 -0500

    Create mergePrices.js

---
+++ mergePrices.js
@@ -0,0 +1,5 @@
+// utility used if you want to expose an aggregated endpoint merging backend1 + backend2
+export function mergeFlat(backend1Array, backend2obj) {
+  // This is a minimal helper if you later want flat merge; for now server.js uses fetchAlbion2D.fetchPrices
+  return { ok: true };
+}
